2fefb47f27db3e42bfaeec5ac2864c43122ef6f7
happyhe <happyhegu@gmail.com>
2018-05-30 14:18:13 +0800

    fix log.go

--- log.go
+++ log.go
@@ -1,36 +1,35 @@
-package log
+package logging
 
 import (
 	"fmt"
-	"github.com/op/go-logging"
 	"os"
 )
 
-var log = logging.MustGetLogger("example")
-var formatFile = logging.MustStringFormatter(
+var logger = MustGetLogger("example")
+var formatFile = MustStringFormatter(
 	`%{time:01-02 15:04:05.000} %{level:.4s} %{shortpkg}.%{shortfunc} %{message}`,
 )
-var formatStd = logging.MustStringFormatter(
+var formatStd = MustStringFormatter(
 	`%{color}%{time:15:04:05.000} %{level:.4s} %{shortpkg}.%{shortfunc} %{color:reset} %{message}`,
 )
 
-func Init(logfilename string, logFileLevel logging.Level) {
+func Init(logfilename string, logFileLevel Level) {
 	logFile, err := os.OpenFile(logfilename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		fmt.Println(err)
 	}
-	backendFile := logging.NewLogBackend(logFile, "", 0)
-	backendStd := logging.NewLogBackend(os.Stdout, "", 0)
+	backendFile := NewLogBackend(logFile, "", 0)
+	backendStd := NewLogBackend(os.Stdout, "", 0)
 
-	backendFileFormatter := logging.NewBackendFormatter(backendFile, formatFile)
-	backendStdFormatter := logging.NewBackendFormatter(backendStd, formatStd)
+	backendFileFormatter := NewBackendFormatter(backendFile, formatFile)
+	backendStdFormatter := NewBackendFormatter(backendStd, formatStd)
 
-	backendFileLeveled := logging.AddModuleLevel(backendFileFormatter)
+	backendFileLeveled := AddModuleLevel(backendFileFormatter)
 	backendFileLeveled.SetLevel(logFileLevel, "")
 
-	logging.SetBackend(backendFileLeveled, backendStdFormatter)
+	SetBackend(backendFileLeveled, backendStdFormatter)
 }
 
-func Log() *logging.Logger {
-	return log
+func Log() *Logger {
+	return logger
 }
